4cb86138e8e576c2a395aaf74be0fe6e5807e174
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-11-23 09:00:21 +0900

    Add __repr__ method

--- sqlitebiter/_counter.py
+++ sqlitebiter/_counter.py
@@ -13,6 +13,10 @@ class ResultCounter(object):
         self.__success_count = 0
         self.__fail_count = 0
 
+    def __repr__(self):
+        return "results: success={:d}, failed={:d}, return_code={:d}".format(
+            self.__success_count, self.__fail_count, self.get_return_code())
+
     def inc_success(self):
         self.__success_count += 1
 
