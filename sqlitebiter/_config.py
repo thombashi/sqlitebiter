69d013b768edabb6bc7dbe78b1f219ea1b49db16
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-08-19 18:06:32 +0900

    Add default encoding parameter to configure subcommand

--- sqlitebiter/_config.py
+++ sqlitebiter/_config.py
@@ -14,6 +14,7 @@ from ._const import PROGRAM_NAME
 
 
 class ConfigKey(object):
+    DEFAULT_ENCODING = "default_encoding"
     PROXY_SERVER = "proxy_server"
     GS_CREDENTIALS_FILE_PATH = "gs_credentials_file_path"
 
@@ -22,10 +23,13 @@ app_config_manager = appconfigpy.ConfigManager(
     config_name=PROGRAM_NAME,
     config_item_list=[
         appconfigpy.ConfigItem(
+            name=ConfigKey.DEFAULT_ENCODING,
+            prompt_text="Default encoding to load files",
+            initial_value="utf-8"),
+        appconfigpy.ConfigItem(
             name=ConfigKey.PROXY_SERVER,
             prompt_text="HTTP/HTTPS proxy server URI",
-            initial_value="",
-        ),
+            initial_value=""),
         # appconfigpy.ConfigItem(
         #    name="gs_credentials_file_path",
         #    prompt_text="Google Sheets credentials file path",
