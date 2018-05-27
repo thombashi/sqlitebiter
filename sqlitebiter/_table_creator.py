a9a3dc9de9624afacc2ba73365dc48b3368d52e4
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-27 15:24:45 +0900

    Rename a variable to be more precisely represent the intended usage

--- sqlitebiter/_table_creator.py
+++ sqlitebiter/_table_creator.py
@@ -20,11 +20,11 @@ class TableCreator(object):
     def create(self, table_data, index_list):
         con_mem = simplesqlite.connect_sqlite_memdb()
         con_mem.create_table_from_tabledata(table_data)
-        is_rename = self.__require_rename_table(con_mem, table_data.table_name)
+        need_rename = self.__require_rename_table(con_mem, table_data.table_name)
         src_table_name = con_mem.get_table_name_list()[0]
         dst_table_name = src_table_name
 
-        if is_rename:
+        if need_rename:
             dst_table_name = self.__make_unique_table_name(src_table_name)
 
             self.__logger.debug(u"rename table from '{}' to '{}'".format(
