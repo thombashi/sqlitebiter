4d9f0d685eb5dff66493cbb6eb3beb887bcc5bae
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-27 22:32:13 +0900

    Fix execution build binary for Windows: #52

--- cli.py
+++ cli.py
@@ -5,8 +5,11 @@
 .. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 """
 
+import multiprocessing
+
 from sqlitebiter.sqlitebiter import cmd
 
 
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
     cmd()
