f36ca44c6d95b91e8bf892660d05f35cef793748
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-06-30 23:31:09 +0900

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -105,6 +105,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Database",
     ],
     entry_points={
