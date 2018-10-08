99c610b18e14ba239d034ffde9a56c9c55701a4b
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-08 23:02:00 +0900

    Update bash completion

--- sqlitebiter/sqlitebiter.py
+++ sqlitebiter/sqlitebiter.py
@@ -331,13 +331,24 @@ def completion(ctx, shell):
             dedent(
                 """\
             _sqlitebiter_completion() {
+                local IFS=$'
+            '
                 COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
                             COMP_CWORD=$COMP_CWORD \
                             _SQLITEBITER_COMPLETE=complete $1 ) )
                 return 0
             }
 
-            complete -F _sqlitebiter_completion -o default sqlitebiter;
+            _sqlitebiter_completionetup() {
+                local COMPLETION_OPTIONS=""
+                local BASH_VERSION_ARR=(${BASH_VERSION//./ })
+                # Only BASH version 4.4 and later have the nosort option.
+                if [ ${BASH_VERSION_ARR[0]} -gt 4 ] || ([ ${BASH_VERSION_ARR[0]} -eq 4 ] && [ ${BASH_VERSION_ARR[1]} -ge 4 ]); then
+                    COMPLETION_OPTIONS="-o nosort"
+                fi
+
+                complete $COMPLETION_OPTIONS -F _sqlitebiter_completion sqlitebiter
+            }
             """
             )
         )
