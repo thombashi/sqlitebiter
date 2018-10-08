bbc7789c3e38e7323ae7f7a73bb177b0c3ff234d
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-08 23:21:39 +0900

    Add a package for build

--- build_package.sh
+++ build_package.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_OS_NAME" = "osx" ] ; then
     bash -x build_macos_binary.sh
 elif [ "$TRAVIS_OS_NAME" = "linux" ] ; then
     sudo apt -qq update
-    sudo apt install -y fakeroot
+    sudo apt install -y fakeroot libczmq-dev
 
     bash -x build_deb_package.sh
 fi
