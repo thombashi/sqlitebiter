0bcca86602aeb5b39256cb5da52d48641d0a9125
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-12-24 18:51:22 +0900

    Add build tox env

--- build_package.sh
+++ build_package.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_OS_NAME" = "osx" ] ; then
     bash -x build_macos_binary.sh
-elif [ "$TRAVIS_OS_NAME" = "linux" ] ; then
+elif [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$TOXENV" = "build" ] ; then
     sudo apt -qq update
     sudo apt install -y fakeroot
     # sudo apt install -y fakeroot libczmq-dev  # 16.04 or later
