aaa480547bf3908a9d27373296ba6a7501101f73
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-12-24 19:03:45 +0900

    Bump version

--- sqlitebiter/__version__.py
+++ sqlitebiter/__version__.py
@@ -6,6 +6,6 @@ from datetime import datetime
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = "Copyright 2016-{}, {}".format(datetime.now().year, __author__)
 __license__ = "MIT License"
-__version__ = "0.23.2"
+__version__ = "0.23.3"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
