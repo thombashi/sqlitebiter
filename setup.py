f883b7662e8f01f8409b32b0bbf4982b85589d06
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-18 12:22:14 +0900

    Add long_description_content_type

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setuptools.setup(
     ],
     license=pkg_info["__license__"],
     long_description=long_description,
+    long_description_content_type="text/x-rst",
     packages=setuptools.find_packages(exclude=["test*"]),
     project_urls={
         "Documentation": "https://{:s}.rtfd.io/".format(MODULE_NAME),
