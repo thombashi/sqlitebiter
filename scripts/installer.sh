e7ece6dce291fcf71ad49da3c464129ed4cc90b3
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-18 12:18:01 +0900

    Add installer script

---
+++ scripts/installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+
+if [ $UID -ne 0 ]; then
+    echo 'requires superuser privilege' 1>&2
+    exit 13
+fi
+
+ARCHIVE_URL=$(curl -sL https://api.github.com/repos/thombashi/sqlitebiter/releases/latest | jq -r '.assets[].browser_download_url' | \grep deb)
+TEMP_DEB="$(mktemp)"
+
+trap "\rm -f $TEMP_DEB" 0 1 2 3 15
+curl -L "$ARCHIVE_URL" -o "$TEMP_DEB"
+dpkg -i "$TEMP_DEB"
