85a01b68489a2cce605610711a31f9edf5feb046
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-18 11:43:38 +0900

    Update gen_homebrew_formula
    - Drop Python 2 support
    - Changes to use retryrequests

--- scripts/gen_homebrew_formula.py
+++ scripts/gen_homebrew_formula.py
@@ -1,40 +1,31 @@
-#!/usr/bin/env python
-# encoding: utf-8
+#!/usr/bin/env python3
 
 """
 .. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 """
 
-from __future__ import print_function, unicode_literals
-
-import io
 import os
 import sys
 from textwrap import indent
 
-from subprocrunner import SubprocessRunner
+import retryrequests
 
 import sqlitebiter
 
 
-def main():
+def main() -> int:
     formula_body = []
 
-    with io.open(
-        os.path.join("docs", "pages", "introduction", "summary.txt"), encoding="utf8"
-    ) as f:
+    with open(os.path.join("docs", "pages", "introduction", "summary.txt"), encoding="utf8") as f:
         formula_body.append('desc "{}"'.format(f.read().strip()))
 
     base_url = "https://github.com/thombashi/{pkg}/releases/download/v{version}".format(
         pkg=sqlitebiter.__name__, version=sqlitebiter.__version__
     )
-
-    proc = SubprocessRunner(
-        "wget {base}/{pkg}_macos_sha256.txt -O -".format(base=base_url, pkg=sqlitebiter.__name__)
+    response = retryrequests.get(
+        "{base}/{pkg}_macos_sha256.txt".format(base=base_url, pkg=sqlitebiter.__name__)
     )
-    if proc.run() != 0:
-        print(proc.stderr, file=sys.stderr)
-        return proc.returncode
+    response.raise_for_status()
 
     formula_body.extend(
         [
@@ -45,7 +36,7 @@ def main():
                 )
             ),
             'version "{}"'.format(sqlitebiter.__version__),
-            'sha256 "{sha256}"'.format(sha256=proc.stdout.split()[0]),
+            'sha256 "{sha256}"'.format(sha256=response.text.split()[0]),
             "",
             "def install",
             '  bin.install "{}"'.format(sqlitebiter.__name__),
