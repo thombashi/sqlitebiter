371f93cf803869b72489e9c46746a2e1ad583c9e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-02-10 01:22:21 +0900

    Bump version

--- sqlitebiter/__version__.py
+++ sqlitebiter/__version__.py
@@ -1,9 +1,6 @@
-# encoding: utf-8
-
-
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = "Copyright 2016, {}".format(__author__)
 __license__ = "MIT License"
-__version__ = "0.28.3"
+__version__ = "0.29.0"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
