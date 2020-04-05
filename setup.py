fdf903c1c7cff1226e8efdfe4ec5dba58d186425
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-04-05 23:23:26 +0900

    Update classifiers

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
         "Topic :: Database",
     ],
     entry_points={"console_scripts": ["sqlitebiter=sqlitebiter.__main__:cmd"]},
