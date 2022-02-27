519b939bca46f1f478e461d4a3c819c99f48ad02
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2022-02-28 00:47:38 +0900

    Add error handling

--- docs/update_command_help.py
+++ docs/update_command_help.py
@@ -8,14 +8,19 @@ import os
 import sys
 from textwrap import dedent, indent
 
-from subprocrunner import SubprocessRunner
+from subprocrunner import CalledProcessError, SubprocessRunner
 
 
 def main() -> int:
     env = dict(os.environ, LC_ALL="C.UTF-8")
 
     proc = SubprocessRunner("sqlitebiter -h")
-    proc.run(env=env, check=True)
+    try:
+        proc.run(env=env, check=True)
+    except CalledProcessError as e:
+        print(f"[ERROR] {e}\n{e.stderr}", file=sys.stderr)
+        sys.exit(1)
+
     assert proc.stdout
     help_file_path = "pages/usage/help.txt"
     print(help_file_path)
