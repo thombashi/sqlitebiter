66d3bc48cb1c41651b7aca1db0b313976664175f
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-01-30 04:00:37 +0900

    Improve the installer script
    - Add integrity check of the archive file
    - Modify to use cache

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -9,10 +9,16 @@ fi
 
 ARCH=$(dpkg --print-architecture)
 VERSION_CODENAME=$(\grep -Po "(?<=VERSION_CODENAME=)[a-z]+" /etc/os-release)
-DEBFILE="${VERSION_CODENAME}_${ARCH}\.deb"
+
+CACHE_DIR_PATH="/var/cache/sqlitebiter"
+RELEASE_ASSETS="${CACHE_DIR_PATH}/latest_release_assets.list"
+CHECKSUMS_FILENAME="sqlitebiter_sha256.txt"
+
+mkdir -p "${CACHE_DIR_PATH}"
+curl -sSL https://raw.githubusercontent.com/thombashi/sqlitebiter/master/scripts/latest_release_assets.list > "${RELEASE_ASSETS}"
 
 set +e
-ARCHIVE_URL=$(curl -sSL https://raw.githubusercontent.com/thombashi/sqlitebiter/master/scripts/latest_release_assets.list | \grep "$DEBFILE")
+ARCHIVE_URL=$(cat "${RELEASE_ASSETS}" | \grep "${VERSION_CODENAME}_${ARCH}\.deb")
 set -e
 
 if [ "$ARCHIVE_URL" = "" ]; then
@@ -20,8 +26,28 @@ if [ "$ARCHIVE_URL" = "" ]; then
     exit 1
 fi
 
-TEMP_DEB="$(mktemp)"
+DEB_FILENAME="$(basename ${ARCHIVE_URL})"
+CACHE_FILE_PATH="${CACHE_DIR_PATH}/${DEB_FILENAME}"
 
-trap "\rm -f $TEMP_DEB" 0 1 2 3 15
-curl -L "$ARCHIVE_URL" -o "$TEMP_DEB"
-dpkg -i "$TEMP_DEB"
+TMP_DIR=$(mktemp -d)
+cd "${TMP_DIR}"
+trap 'rm -rf ${TMP_DIR}' 0 1 2 3 15
+
+if [ -f "${CACHE_FILE_PATH}" ]; then
+    echo "found local cache: ${DEB_FILENAME}"
+    cp -a "${CACHE_FILE_PATH}" .
+else
+    echo "downloading a deb file ..."
+    curl -fL --progress-bar "${ARCHIVE_URL}" -o "${DEB_FILENAME}"
+fi
+
+echo "check the file integrity ..."
+CHECKSUMS_URL=$(cat "${RELEASE_ASSETS}" | \grep "${CHECKSUMS_FILENAME}")
+curl -fsSL "${CHECKSUMS_URL}" -o "${CHECKSUMS_FILENAME}"
+\grep "${DEB_FILENAME}" "${CHECKSUMS_FILENAME}" | sha256sum -c -
+
+echo "installing ..."
+dpkg -i "${DEB_FILENAME}"
+
+# Replace the local cache file
+cp -a "${DEB_FILENAME}" "${CACHE_FILE_PATH}"
