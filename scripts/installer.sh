513f9f0b9c0f36ab56d623fbfeef912af25939c9
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-01-30 03:22:17 +0900

    Remove dependency from the installer script to GitHub-API/jq

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -11,7 +11,7 @@ ARCH=$(dpkg --print-architecture)
 VERSION_CODENAME=$(\grep -Po "(?<=VERSION_CODENAME=)[a-z]+" /etc/os-release)
 DEBFILE="${VERSION_CODENAME}_${ARCH}\.deb"
 
-ARCHIVE_URL=$(curl -sSL https://api.github.com/repos/thombashi/sqlitebiter/releases/latest | jq -r '.assets[].browser_download_url' | \grep "$DEBFILE")
+ARCHIVE_URL=$(curl -sSL https://raw.githubusercontent.com/thombashi/sqlitebiter/master/scripts/latest_release_assets.list | \grep "$DEBFILE")
 TEMP_DEB="$(mktemp)"
 
 trap "\rm -f $TEMP_DEB" 0 1 2 3 15
