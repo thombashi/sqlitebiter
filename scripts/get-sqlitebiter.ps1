e8418d59484c289299a7a15e58e12ddf6a68f8d0
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-02-10 11:51:07 +0900

    Fix archive file name in get-sqlitebiter.ps1 script: #102

--- scripts/get-sqlitebiter.ps1
+++ scripts/get-sqlitebiter.ps1
@@ -1,7 +1,7 @@
 $package = "sqlitebiter"
 $latest_release = Invoke-WebRequest https://github.com/thombashi/$package/releases/latest -Headers @{"Accept"="application/json"} | ConvertFrom-Json
 $version = $latest_release.tag_name
-$archive = "sqlitebiter_win_x64.zip"
+$archive = "sqlitebiter_windows_amd64.zip"
 
 wget https://github.com/thombashi/$package/releases/download/$version/$archive -OutFile $archive
 Expand-Archive -Path $archive -DestinationPath .
